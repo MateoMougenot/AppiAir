019fa4d7db63297ddb29ef987ac6b65d68946eb0
MateoMougenot <mateo.mougenot@etu.unilim.fr>
2025-09-17 08:48:03 +0200

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  base: "/AppiAir",
+  site: "https://mateo.mougenot.github.io",
 });
